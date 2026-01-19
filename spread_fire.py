de14a805921ea7957ce1c5b03972082027d2def3
michael111999 <micsasson7@gmail.com>
2026-01-19 17:20:37 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -22,4 +22,22 @@ def spread_fire(grid):
     grid = np.where(grid < p_tree, 1, 0)
     grid[grid_size // 2][grid_size // 2] = 2
     return grid
+    # Set up the grid
+grid_size = 30
+p_tree = 0.6  # Probability that a cell contains a tree
+
+grid = initialize_forest(grid_size, p_tree)
+
+# run the simulation
+fig, ax = plt.subplots()
+for i in range(100):
+    update_grid = spread_fire(grid)
+    if np.array_equal(update_grid, grid):
+        break
+    grid = update_grid
+    ax.imshow(grid, cmap='YlOrRd', vmin=0, vmax=2)
+    ax.set_title(f'Step {i}')
+    display(fig)
+    clear_output(wait = True)
+    plt.pause(0.01)
 
